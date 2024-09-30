53fb2f70fdcd59902adf0b4180186b4e397fb678
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-30 22:44:13 +0800

    created definitions component to render definitions

---
+++ src/Definitions.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Definitions(props) {
+  console.log(props.meanings);
+
+  return (
+    <div className="Definitions">
+      <h3>
+        {props.meanings.partOfSpeech} | {props.phonetic}
+      </h3>
+      {props.meanings.definitions.map(function (definition, index) {
+        // definition = props.meanings.definitions
+        if (index < 3) {
+          return (
+            <div key={index}>
+              <p>{definition.definition}</p>
+              {definition.example && (
+                <p>
+                  <strong>example</strong> {definition.example}
+                </p>
+              )}
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,22 +1,27 @@
 import React from "react";
+import Definitions from "./Definitions";
 
 export default function Results(props) {
   console.log(props.result);
+  const phonetic = props.result.phonetic;
 
   if (props.result) {
     return (
       <div className="Results">
         <h1>{props.result.word}</h1>
-        <div className="definitions">
-          <h2>oxford dictionary</h2>
-          <h3>noun | pro.nun.cia.tion</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            vehicula, odio quis condimentum imperdiet, nisl risus iaculis neque,
-            faucibus facilisis orci mi at mi. Aliquam quis mi est.
-          </p>
+        <div className="definitions-container">
+          <h2>wikitionary dictionary</h2>
+          {props.result.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Definitions meanings={meaning} phonetic={phonetic} />
+              </div>
+            );
+          })}
         </div>
+
         <hr />
+        <div></div>
       </div>
     );
   } else {
