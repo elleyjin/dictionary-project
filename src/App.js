0192d8fb19abfe54087d982f7933ce290418fd6c
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-30 23:45:43 +0800

    added github source code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
       <header className="App-header">
         <Dictionary defaultWord="sunrise" />
       </header>
+      <footer>
+        <a
+          href="https://github.com/elleyjin/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>{" "}
+        by ElleyJin
+      </footer>
     </div>
   );
 }
