5ac83324eb9abf1f7ca3de668641832ce1558882
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-30 14:30:05 +0800

    created skeleton layout

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -25,6 +25,17 @@ export default function Dictionary() {
           <button className="btn btn-outline-dark">Chi</button>
         </div>
       </nav>
+      <h1>sunset</h1>
+      <div>
+        <h2>oxford dictionary</h2>
+        <h3>noun | pro.nun.cia.tion</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          vehicula, odio quis condimentum imperdiet, nisl risus iaculis neque,
+          faucibus facilisis orci mi at mi. Aliquam quis mi est.
+        </p>
+      </div>
+      <hr />
     </div>
   );
 }
