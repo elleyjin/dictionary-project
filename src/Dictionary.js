b0dc862318c5edc60bf190ca4932c4f840654bd9
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-30 14:27:16 +0800

    added navbar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,14 +2,28 @@ import React from "react";
 
 export default function Dictionary() {
   return (
-    <div>
-      <nav>
-        <form>
-          <input type="search" placeholder="Search word" />
-          <button type="submit" placeholder="search">
+    <div className="Dictionary container-fluid">
+      <nav className="navbar navbar-light bg-light ">
+        <form className="form-inline d-flex">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search word"
+          />
+          <button
+            className="btn btn-outline-dark my-2 my-sm-0"
+            type="submit"
+            placeholder="search"
+          >
             Search
           </button>
         </form>
+        <div className="Languages">
+          <button className="btn btn-outline-dark">Eng</button>
+          {/* another components */}
+          <button className="btn btn-outline-dark">Kor</button>
+          <button className="btn btn-outline-dark">Chi</button>
+        </div>
       </nav>
     </div>
   );
