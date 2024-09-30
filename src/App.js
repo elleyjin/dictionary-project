56f25a337ac7bbc0101f46b362aedfc92abea042
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-30 14:11:00 +0800

    created dictionary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // import logo from "./logo.svg";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Dictionary />
+      </header>
     </div>
   );
 }
